55ecd498c3912087341113438da63949fc03d038
Danielle Voznyy <dan.voznyy@gmail.com>
2024-05-02 17:41:46 -0400

    refactor: Swap atomifcfu for touchlab/Stately due to JVM 21 issues

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,7 @@
+plugins {
+    idea
+}
+
 val includedProjects: String = file(".include")
     .readLines()
     .first()
@@ -10,6 +14,22 @@ fun taskFromChildren(taskName: String) {
     }
 }
 
+repositories {
+    mavenCentral()
+    google()
+    maven("https://repo.mineinabyss.com/releases")
+    maven("https://repo.mineinabyss.com/snapshots")
+    maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://repo.dmulloy2.net/repository/public/")
+
+}
 taskFromChildren("build")
 taskFromChildren("clean")
 taskFromChildren("assemble")
+
+idea {
+    module {
+        isDownloadJavadoc = true
+        isDownloadSources = true
+    }
+}
